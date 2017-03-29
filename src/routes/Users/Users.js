bb301862bbadd2796cc77e2ae8d6a2bb2b2ec5e8
renzhao <renzhao@vpgame.cn>
2017-03-29 18:04:22 +0800

    用户管理

--- src/routes/Users/Users.js
+++ src/routes/Users/Users.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { connect } from 'dva'
 import './style.less'
+import UsersComponent from '../../components/Users/Users'
 
 function Users() {
   return (
-    <div className="normal">
-      Route Component: Users
+    <div className="box container">
+      <UsersComponent />
     </div>
   )
 }
 
-function mapStateToProps() {
-  return {}
-}
-
-export default connect(mapStateToProps)(Users)
+export default connect()(Users)
